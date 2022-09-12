ade43f5b151734e97b971037c931bd5810ab70c2
Dmytro Trotsenko <trohae7@gmail.com>
2022-09-12 10:32:36 +0300

    Fixed HomeWorkApp

--- src/ua/hillelit/lms/homeWork/HomeWorkApp.java
+++ src/ua/hillelit/lms/homeWork/HomeWorkApp.java
@@ -4,20 +4,26 @@ public class HomeWorkApp {
 
   public static void main(String[] args) {
 
-    System.out.println(findSymbolOccurance("qwertyqwq", 'q'));
+    System.out.println("Symbol Occurance = " + findSymbolOccurance(null, 'q'));
 
-    System.out.println(findWordPosition("Apollo", "pollo"));
+    System.out.println("Word Position = " + findWordPosition("Apollo", null));
 
-    System.out.println(stringReverse("Hello"));
+    System.out.println("Reverse word: " + stringReverse("Hello"));
 
-    System.out.println(isPalindrome("aErea"));
+    System.out.println("Is Palindrome: " + isPalindrome("eRE"));
 
   }
 
   public static int findSymbolOccurance(String str, char symbol) {
 
-    char[] charArr = str.toCharArray();
     int count = 0;
+
+    if (str == null) {
+      System.out.println("[Warning! \"String str == null\"]");
+      return count;
+    }
+
+    char[] charArr = str.toCharArray();
 
     for (char chars : charArr) {
 
@@ -32,45 +38,38 @@ public class HomeWorkApp {
 
   public static int findWordPosition(String source, String target) {
 
+    if (source == null || target == null) {
+      System.out.println("[Warning! \"String source == null or target == null\"]");
+      return -1;
+    }
+
     return source.indexOf(target);
 
   }
 
   public static String stringReverse(String str) {
 
-    char[] charArr = str.toCharArray();
-    int len = charArr.length - 1;
-    StringBuilder reverseStr = new StringBuilder();
-
-    for (int i = len; i >= 0; i--) {
-      reverseStr.append(charArr[i]);
+    if (str == null) {
+      return "[Warning! \"String str == null\"]";
     }
-    return reverseStr.toString();
+
+    StringBuilder newStr = new StringBuilder(str);
+
+    return newStr.reverse().toString();
 
   }
 
   public static boolean isPalindrome(String str) {
 
-    str = str.toLowerCase();
-
-    char[] charArr = str.toCharArray();
-
-    int len = charArr.length;
-    int leftIndex = 0;
-    int rightIndex = len - 1;
-
-    if (len % 2 == 0) {
+    if (str == null) {
+      System.out.println("[Warning! \"String str == null\"]");
       return false;
     }
 
-    while (leftIndex < len / 2) {
+    str = str.toLowerCase();
+    String strReverse = stringReverse(str);
 
-      if (charArr[leftIndex++] != charArr[rightIndex--]) {
-        return false;
-      }
-
-    }
-    return true;
+    return str.equals(strReverse);
 
   }
 
